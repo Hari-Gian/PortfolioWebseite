57d7494a37d8dfbd8916cf9f904ec50e96f8dffe
Hari-Gian <harigian545@gmail.com>
2025-05-31 22:45:17 +0200

    weiter gearbeitet

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,14 @@
 export default function Footer() {
     return (
-        <footer className="bg-white relative w-full bg-gradient-to-r text-gray-100 pt-20 pb-8 mt-8 shadow-lg">
+        <footer className="bg-black relative w-full bg-gradient-to-r text-gray-100 pt-20 pb-8 mt-8 border-t shadow-sm">
 
 
             <div
                 className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
 
 
-                <div className="flex flex-col justify-center items-center space-y-4 text-black">
-                    <a className="text-black transition-colors duration text-lg font-extrabold text-xl">Contact:</a>
+                <div className="flex flex-col justify-center items-center space-y-4 text-gray-400">
+                    <a className="text-gray-300 transition-colors duration font-extrabold text-2xl">Contact:</a>
                     <a
                         href="https://github.com"
                         target="_blank"
@@ -33,8 +33,8 @@ export default function Footer() {
                     </a>
                 </div>
 
-                <div className="flex flex-col justify-center items-center space-y-4 text-black ">
-                    <a className="text-black transition-colors duration text-lg font-extrabold text-xl">Programmiersprachen:</a>
+                <div className="flex flex-col justify-center items-center space-y-4 text-gray-400 ">
+                    <a className="text-gray-300 transition-colors duration font-extrabold text-2xl">Programmiersprachen:</a>
                     <a
                         href="https://tailwindcss.com"
                         target="_blank"
@@ -54,8 +54,8 @@ export default function Footer() {
                 </div>
 
                 <div className="text-center md:text-left">
-                    <h2 className="text-2xl font-extrabold text-black ">Portfolio Webseite</h2>
-                    <p className="text-sm text-black">
+                    <h2 className="text-2xl font-extrabold text-gray-400 ">Portfolio Webseite</h2>
+                    <p className="text-sm text-gray-300">
                         &copy; {new Date().getFullYear()} Alle Rechte vorbehalten.
                     </p>
                 </div>
@@ -63,13 +63,13 @@ export default function Footer() {
                 <div className="flex flex-col space-y-4">
                     <a
                         href="#impressum"
-                        className="text-black text-lg hover:text-white transition-colors duration-300"
+                        className="text-gray-400 text-lg hover:text-white transition-colors duration-300"
                     >
                         Impressum
                     </a>
                     <a
                         href="#datenschutz"
-                        className="text-black text-lg hover:text-white transition-colors duration-300"
+                        className="text-gray-400 text-lg hover:text-white transition-colors duration-300"
                     >
                         Datenschutz
                     </a>
